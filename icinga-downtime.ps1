d666f4e9deba445106fefb16155568a6e2825d3d
Markus Frosch <markus.frosch@netways.de>
2021-06-29 20:00:04 +0200

    Add copyright info into script

--- icinga-downtime.ps1
+++ icinga-downtime.ps1
@@ -12,6 +12,21 @@
     .\icinga-downtime.ps1 -HostName "this-windows-host.fqdn" -Author "Windows" -Comment "Automatic shutdown downtime";
 .LINK
 https://github.com/NETWAYS/icinga-downtime-scripts
+.NOTES
+Copyright (C) 2021 NETWAYS GmbH <info@netways.de>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #>
 param (
     [String]$Api = "https://icinga.local:5665",
